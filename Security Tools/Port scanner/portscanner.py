f21c199ec5a866d48fdedc888cde0cb8c28aaeac
Javier Ferrándiz Fernández <96439507+javisys@users.noreply.github.com>
2023-10-23 18:14:32 +0200

    Update portscanner.py

--- Security Tools/Port scanner/portscanner.py
+++ Security Tools/Port scanner/portscanner.py
@@ -1,17 +1,25 @@
-#Javi_16018
+#Javi_16018 - Mod 23/10/2023
 import socket
 
-# IP address of the host you want to scan
-ip = "192.168.1.1"
-
 # Runs through each port from 1 to 65535
-for port in range(1, 65536):
+def scan_ports(ip, start_port, end_port):
+    for port in range(start_port, end_port + 1):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
     result = s.connect_ex((ip, port))
 
     if result == 0:
-        print(f"Puerto abierto: {port}")
+        print(f"Open port: {port}")
 
     # Close the socket
     s.close()
+
+
+# IP address of the host you want to scan
+ip = "192.168.1.1"
+
+# Define the range of ports you want to scan
+start_port = 1
+end_port = 65535
+
+scan_ports(ip, start_port, end_port)
